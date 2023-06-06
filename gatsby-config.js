c5a7f2e1b0f1cfc7862fbbe37d5801dc8c6efccb
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-06-06 15:28:53 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,44 +55,33 @@ module.exports = {
         siteUrl: `https://www.soulbreezebackpackers.com`,
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-google-gtag`,
-    //   options: {
-
-    //     trackingIds: [
-    //       "UA-200252399-2", // Google Analytics / GA
-
-    //     ],
-    //     // This object gets passed directly to the gtag config command
-    //     // This config will be shared across all trackingIds
-    //     gtagConfig: {
-    //       optimize_id: "OPT_CONTAINER_ID",
-    //       anonymize_ip: true,
-    //       cookie_expires: 0,
-    //     },
-    //     // This object is used for configuration specific to this plugin
-    //     pluginConfig: {
-    //       // Puts tracking script in the head instead of the body
-    //       head: true,
-    //       // Setting this parameter is also optional
-    //       respectDNT: true,
-    //       // Avoids sending pageview hits from custom paths
-    //       exclude: ["/preview/**", "/do-not-track/me/too/"],
-    //     },
-    //   },
-    // },
     {
-      resolve: `gatsby-plugin-gdpr-cookies`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        googleAnalytics: {
-          trackingId:    "UA-200252399-2", // Google Analytics / GA
-          // Setting this parameter is optional
-          anonymize: true
+
+        trackingIds: [
+          "UA-200252399-2", // Google Analytics / GA
+
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
         },
-        // Defines the environments where the tracking should be available  - default is ["production"]
-        environments: ['production', 'development']
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
       },
     },
+ 
 
 
     {
